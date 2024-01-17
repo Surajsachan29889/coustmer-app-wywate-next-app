7b8fbb704c39cf36b5a69f50661bd9eadf21509e
suraj sachan <100021123+Surajsachan29889@users.noreply.github.com>
2024-01-17 16:19:31 +0530

    #

--- app/page.js
+++ app/page.js
@@ -4,10 +4,10 @@ export default function App() {
   return (
     <div className=' flex justify-center items-center flex-col gap-[5vh] pt-[20%]  '>
         <Link href="/checkout">
-            <button className='w-[10vw] h-[10vh]  text-[white] font-bold rounded-xl bg-pri  '>checkout Screen</button>
+            <button className='w-[100%] p-[5%] h-[10vh]  text-[white] font-bold rounded-xl bg-pri  '>checkout Screen</button>
         </Link>
         <Link href="/screen20">
-            <button className='w-[10vw] h-[10vh]  text-[white] font-bold rounded-xl bg-pri  '>Screen 20</button>
+            <button className='w-[100%] p-[5%] h-[10vh]  text-[white] font-bold rounded-xl bg-pri  '>Screen 20</button>
         </Link>
     </div>
   )
